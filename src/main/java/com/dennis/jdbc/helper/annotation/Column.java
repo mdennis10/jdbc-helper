4d6f8e85c3a5390b87aae46fd59dd97eafa4f62e
Mario Dennis <mdennis@mcsystems.com>
2018-06-20 19:50:47 -0500

    add documentation for Column annotation

--- src/main/java/com/dennis/jdbc/helper/annotation/Column.java
+++ src/main/java/com/dennis/jdbc/helper/annotation/Column.java
@@ -5,6 +5,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Column annotation is used to map
+ * property to database column
+ * @author Mario Dennis
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface Column {
